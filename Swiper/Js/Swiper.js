5ca56bbda2b64835c71af99e22edcfd1bd6700df
FL <1458174276@qq.com>
2020-05-09 16:52:39 +0800

    可以首位衔接

--- Swiper/Js/Swiper.js
+++ Swiper/Js/Swiper.js
@@ -1,17 +1,23 @@
- let sliderwidth = 0;
- let delay = 1000;
- let currentIndex = 1;
- let Cycle = true
- let pagedelay = '300ms'
- let length = 0;
- let clickIndex = 0;
- let clicktime = 1000
+ let sliderwidth = 0; //当前需轮播的宽度
+ let delay = 1000; // 每次轮播之间延迟的时间
+ let currentIndex = 1; // 当前所在的位置
+ let Cycle = true // 是否开启轮播
+ let pagedelay = '300ms' // 轮播切换的动画延迟时间
+ let length = 0; // 有多少个需轮播的图片
+ let clickIndex = 0; // 当前点击的元素索引
+ let clicktime = 1000 // 点击之后的停顿时间
+ let flag = false
  $(function () {
    sliderwidth = $('.slider').width()
    //  Cycle = false
+   //  复制第一个放到最后
+   $($('.slider')[0]).clone().appendTo('.swiperchild')
    length = $('.slider').length
+   //  启动轮播
    Cyclefnc(Cycle)
-   for (let i = 0; i < length; i++) {
+   //  创建指示点到图片上
+   // 减一是减去最后一张图片
+   for (let i = 0; i < length - 1; i++) {
      //  $('.indicator').appendTo('<div></div>')
      $('<div class="slideritem"></div>').appendTo('.indicator');
    }
@@ -27,38 +33,43 @@
    $('.indicator>div').click(function () {
      let index = $(this).index()
      currentIndex = index;
-      Cycle = false
-      setTimeout(function(){
-        Cycle = true
-        Cyclefnc()
-      },clicktime)
+     Cycle = false
+     setTimeout(function () {
+       Cycle = true
+       Cyclefnc()
+     }, clicktime)
    })
  });
 
  function Cyclefnc() {
    //  while (Cycle) {
    let timer = 0
-   if (Cycle) {
-     timer = setTimeout(function () {
-      sliper()
 
+   if (Cycle) {
+     let temp =  delay
+     if ((currentIndex == 1) && flag){
+       temp = 0;
+     }
+     timer = setTimeout(function () {
+       sliper()
        clearInterval(timer)
        Cyclefnc()
+       flag = true
        //  Cycle?Cyclefnc():0
-     }, delay)
+     }, temp)
      //  }
    } else
      clearInterval(timer || 0)
  }
 
  function sliper(replacewidth = 0) {
-   let slider = replacewidth ? replacewidth:(-sliderwidth * (currentIndex))
+   let slider = replacewidth ? replacewidth : (-sliderwidth * (currentIndex))
    $('.swiperchild').css({
      'transform': `translateX(${slider}px)`,
      //  'transition': 'transfrom 300ms ease 0s'
      'transition': currentIndex == 0 ? '0s' : pagedelay
    })
-   $($('.slideritem')[currentIndex]).toggleClass('active')
+   $($('.slideritem')[currentIndex]).addClass('active')
    $('.slideritem').filter((index, item) => {
      // return  !$(item).hasClass('active')
      return index != currentIndex
@@ -66,6 +77,7 @@
    //  console.log(arr);
    currentIndex += 1;
    if (currentIndex == length) {
-     currentIndex = 0;
+      currentIndex = 0;
+     $($('.slideritem')[currentIndex]).addClass('active')
    }
  }
