f59783744d6d1f1d6338c1d7a5e52ba4c531705b
FL <1458174276@qq.com>
2020-05-09 10:04:00 +0800

    页面切换有过渡效果

--- Swiper/Js/Swiper.js
+++ Swiper/Js/Swiper.js
@@ -2,6 +2,7 @@
  let delay = 1000;
  let currentIndex = 1;
  let Cycle = false
+ let pagedelay='300ms'
  $(function () {
    sliderwidth = $('.slider').width()
    Cycle = true
@@ -14,7 +15,8 @@
    let timer = setTimeout(function () {
      $('.swiperchild').css({
        'transform': `translateX(${(-sliderwidth*currentIndex)}px)`,
-       'transition': 'transfrom 300ms ease 0s'
+      //  'transition': 'transfrom 300ms ease 0s'
+      'transition': currentIndex == 0 ? '0s' : pagedelay
      })
      currentIndex += 1;
      if (currentIndex == $('.slider').length) {
