872202080d66835b03bba2d7f37f723d77b14404
FL <1458174276@qq.com>
2020-05-09 15:36:46 +0800

    点击指示点可以移动

--- Swiper/Js/Swiper.js
+++ Swiper/Js/Swiper.js
@@ -4,6 +4,8 @@
  let Cycle = true
  let pagedelay = '300ms'
  let length = 0;
+ let clickIndex = 0;
+ let clicktime = 1000
  $(function () {
    sliderwidth = $('.slider').width()
    //  Cycle = false
@@ -15,6 +17,22 @@
    }
 
    $($('.slideritem')[0]).addClass('active')
+
+
+   //  $('slideritem').map((item,index=>{
+   //    $(item).click(e=>{
+   //       console.log();
+   //    })
+   //指示点被点击
+   $('.indicator>div').click(function () {
+     let index = $(this).index()
+     currentIndex = index;
+      Cycle = false
+      setTimeout(function(){
+        Cycle = true
+        Cyclefnc()
+      },clicktime)
+   })
  });
 
  function Cyclefnc() {
@@ -22,21 +40,7 @@
    let timer = 0
    if (Cycle) {
      timer = setTimeout(function () {
-       $('.swiperchild').css({
-         'transform': `translateX(${(-sliderwidth*(currentIndex))}px)`,
-         //  'transition': 'transfrom 300ms ease 0s'
-         'transition': currentIndex == 0 ? '0s' : pagedelay
-       })
-       $($('.slideritem')[currentIndex]).toggleClass('active')
-       $('.slideritem').filter((index, item) => {
-         // return  !$(item).hasClass('active')
-         return index != currentIndex
-       }).removeClass('active')
-       //  console.log(arr);
-       currentIndex += 1;
-       if (currentIndex == length) {
-         currentIndex = 0;
-       }
+      sliper()
 
        clearInterval(timer)
        Cyclefnc()
@@ -45,4 +49,23 @@
      //  }
    } else
      clearInterval(timer || 0)
+ }
+
+ function sliper(replacewidth = 0) {
+   let slider = replacewidth ? replacewidth:(-sliderwidth * (currentIndex))
+   $('.swiperchild').css({
+     'transform': `translateX(${slider}px)`,
+     //  'transition': 'transfrom 300ms ease 0s'
+     'transition': currentIndex == 0 ? '0s' : pagedelay
+   })
+   $($('.slideritem')[currentIndex]).toggleClass('active')
+   $('.slideritem').filter((index, item) => {
+     // return  !$(item).hasClass('active')
+     return index != currentIndex
+   }).removeClass('active')
+   //  console.log(arr);
+   currentIndex += 1;
+   if (currentIndex == length) {
+     currentIndex = 0;
+   }
  }
